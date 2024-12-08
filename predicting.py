5ad96baefe06ae04afd2922753537020f480224a
walking_fires <zqop2013@gmail.com>
2024-12-08 20:36:17 +0300

    streamlit app doc strings

--- predicting.py
+++ predicting.py
@@ -1,15 +1,44 @@
+import logging
 import os
-
 import joblib
 import pandas as pd
+
 class Predictor:
+    """
+    Класс Predictor предназначен для предсказания на основе загруженной модели.
+
+    Атрибуты:
+    - classifier: Загруженная модель
+
+    Методы:
+    - __init__(self, model: str): Инициализация класса Predictor
+    - predict(self, data: dict, **kwargs): Предсказание на основе входных данных
+    """
 
     def __init__(self, model: str):
+        """
+        Инициализация класса Predictor
+
+        Параметры:
+        - model: Название модели, используемой для предсказания
+        """
+        logging.info("Predictor: Loading model")
         if isinstance(model, str) and os.path.isfile(f'models/{model}_model.joblib'):
             self.classifier = joblib.load(f'models/{model}_model.joblib')
         else:
-            raise ValueError('Classifier: Model not found.')
+            logging.error('Predictor: Model not found')
+            raise ValueError('Predictor: Model not found.')
 
-    def predict(self, data: dict, **kwargs):
+    def predict(self, data: dict):
+        """
+        Предсказание на основе входных данных
+
+        Параметры:
+        - data: Входные данные для предсказания
+
+        Возвращает:
+        - prediction: Предсказание
+        """
+        logging.info("Predictor: Predicting")
         prediction = self.classifier.predict(pd.DataFrame([data]))
         return prediction[0]
